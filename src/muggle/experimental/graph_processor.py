78427f8d18d243047f706204bef12ec47bd428c1
Edward Wu <robotmice@gmail.com>
2026-05-03 13:01:45 +0000

    feat: implement insurance inquiry workflow in GraphProcessor

--- src/muggle/experimental/graph_processor.py
+++ src/muggle/experimental/graph_processor.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import Annotated, Sequence
 
 import pydash
@@ -13,12 +14,21 @@ from pydantic import BaseModel, Field
 
 from muggle.core.processor import ProcessorInterface
 from muggle.infra.registry import ModelRegistry, PromptRegistry
-from muggle.shared.constants import STR_PROMPT_INGEST, STR_LLM_DEFAULT, STR_NODE_INGEST, STR_PROMPT_DEFAULT
+from muggle.shared.constants import STR_PROMPT_INGEST, STR_LLM_DEFAULT, STR_NODE_INGEST, STR_NODE_UNHANDLED, STR_PROMPT_INSURANCE, STR_NODE_INSURANCE
 
 
 class WorkflowState(BaseModel):
     messages: Annotated[list, add_messages, Field(default_factory=list)]
-    type: str | None = None
+    type: Annotated[str | None, Field(description="")] = None
+    response: Annotated[str | None, Field(description="Response of the inquiry")] = None
+
+
+class IngestResult(BaseModel):
+    type: Annotated[str | None, Field(description="Result of the analysis")] = None
+
+
+class InsuranceInquiryResult(BaseModel):
+    response: Annotated[str | None, Field(description="Response of the inquiry")] = None
 
 
 def simple_human_message(messages: list[str]):
@@ -33,9 +43,16 @@ def config_map(thread_id: str | None = None) -> RunnableConfig:
     )
 
 
+def ingest_router(state: WorkflowState) -> str:
+    if state.type == "insurance":
+        return STR_NODE_INSURANCE
+    else:
+        return STR_NODE_UNHANDLED
+
+
 def unhandled_response_node(state: WorkflowState):
     _ = state
-    return simple_human_message(["I cannot answer this question."])
+    return {"response": "I cannot respond to this question."}
 
 
 class GraphProcessor(ProcessorInterface):
@@ -46,24 +63,41 @@ class GraphProcessor(ProcessorInterface):
         self._ready = False
         self._last_error = None
 
-        self.agent_ingest = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_INGEST))
-        self.agent2 = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_DEFAULT))
-        self.agent3 = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_DEFAULT))
-        self.agent4 = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_DEFAULT))
+        def ingest_node(state: WorkflowState):
+            state = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_INGEST),
+                                 response_format=IngestResult).invoke(state)
+
+            return {"type": pydash.get(state, "structured_response.type"),
+                    "messages": pydash.get(state, "messages")}
+
+        def insurance_inquiry_node(state: WorkflowState):
+            state = create_agent(model=registry.get_model(default_model), system_prompt=prompt_registry.get_system_prompt(STR_PROMPT_INSURANCE),
+                                 response_format=InsuranceInquiryResult).invoke(state)
+
+            return {"response": pydash.get(state, "structured_response.response"),
+                    "messages": pydash.get(state, "messages")}
 
         graph_builder = StateGraph(WorkflowState)
-        graph_builder.add_node(STR_NODE_INGEST, self.agent_ingest)
+        graph_builder.add_node(STR_NODE_INGEST, ingest_node)
+        graph_builder.add_node(STR_NODE_INSURANCE, insurance_inquiry_node)
+        graph_builder.add_node(STR_NODE_UNHANDLED, unhandled_response_node)
+
         graph_builder.add_edge(START, STR_NODE_INGEST)
-        graph_builder.add_edge(STR_NODE_INGEST, END)
+
+        graph_builder.add_conditional_edges(STR_NODE_INGEST, ingest_router, [STR_NODE_INSURANCE, STR_NODE_UNHANDLED])
+
+        graph_builder.add_edge(STR_NODE_INSURANCE, END)
+        graph_builder.add_edge(STR_NODE_UNHANDLED, END)
+
         self.workflow = graph_builder.compile(checkpointer=InMemorySaver())
+        self.workflow.get_graph().print_ascii()
 
     def get_response(self, message: str, thread_id: str | None = None, stream_mode: StreamMode | Sequence[StreamMode] | None = None) -> str:
         for chunk in self.workflow.stream(simple_human_message([message]), config=config_map(thread_id), stream_mode=stream_mode):
-            print(chunk)
-            if self._last_error is None:
-                self._last_error = chunk
-        state = self.workflow.get_state(config=config_map())
-        return pydash.get(state, "values.messages[-1].content", "")
+            pprint(chunk)
+
+        state = self.workflow.get_state(config=config_map(thread_id))
+        return pydash.get(state.values, "response", "")
 
     def warm_up(self):
         """Perform internal initialization and validation."""
--- src/muggle/shared/constants.py
+++ src/muggle/shared/constants.py
@@ -5,9 +5,19 @@ STR_NODE_INGEST = "node-ingest"
 STR_AGENT_INGEST = "agent-ingest"
 STR_PROMPT_INGEST = "prompt-ingest"
 
+STR_NODE_INSURANCE = "node-insurance-inquiry"
+STR_AGENT_INSURANCE = "agent-insurance-inquiry"
+STR_PROMPT_INSURANCE = "prompt-insurance-inquiry"
+
 STR_NODE_VALIDATE = "node-validate"
 STR_AGENT_VALIDATE = "agent-validate"
 STR_PROMPT_VALIDATE = "prompt-validate"
+
+STR_NODE_OUTPUT = "node-output"
+
+STR_NODE_UNHANDLED = "node-unhandled"
+STR_AGENT_UNHANDLED = "agent-unhandled"
+STR_PROMPT_UNHANDLED = "prompt-unhandled"
 
 STR_NODE_X = "node-x"
 STR_AGENT_X = "agent-x"
